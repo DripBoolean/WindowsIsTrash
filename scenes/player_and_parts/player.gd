be47e9c3cf7800c4b03ca682a4fa712a2f95bab4
Simon Rhodes <93493041+DripBoolean@users.noreply.github.com>
2024-03-22 19:02:51 -0400

    Added a mofukin uhhh
    yup

---
+++ scenes/player_and_parts/player.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+var camera_rotation = Vector2(0, 0)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		camera_rotation += event.relative * -0.05
+		camera_rotation.y = clamp(camera_rotation.y, -PI / 2, PI / 2)
+		$Camera.transform.basis = Basis() # reset rotation
+		$Camera.rotate_object_local(Vector3(0, 1, 0), camera_rotation.x)
+		$Camera.rotate_object_local(Vector3(1, 0, 0), camera_rotation.y)
+		
