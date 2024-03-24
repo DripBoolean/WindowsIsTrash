c1405d714f7932d8670565a575b08b00895b56ec
Simon Rhodes <93493041+DripBoolean@users.noreply.github.com>
2024-03-24 10:25:02 -0400

    Made purple guy killable

--- scenes/enemy_and_parts/enemy.gd
+++ scenes/enemy_and_parts/enemy.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+var health = 3
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var player = get_tree().get_nodes_in_group("player")[0]
@@ -8,4 +10,8 @@ func _process(delta):
 	
 	position += movement_direction * 0.02
 	
+	if health <= 0:
+		queue_free()
 	
+func take_damage():
+	health -= 1
