acbfa8c496a282075661d2e98cabc7564b3a8d02
Simon Rhodes <93493041+DripBoolean@users.noreply.github.com>
2024-03-29 14:02:10 -0400

    added progress bar

--- scenes/world_parts/terminal_tracker.gd
+++ scenes/world_parts/terminal_tracker.gd
@@ -7,9 +7,19 @@ var won = false
 func _process(_delta):
 	if won:
 		return
+	
+		
+	
+	var num_terminals = get_tree().get_nodes_in_group("progress").size()
+	var num_completed = 0
 	for node in get_tree().get_nodes_in_group("progress"):
-		if not node.complete():
-			return
+		if node.complete():
+			num_completed += 1
+	
+	
+	$Node2D/ProgressBar.value = 100 * float(num_completed) / float(num_terminals)
 
-	won = true
-	add_child(winscreen.instantiate())
+	
+	if num_completed == num_terminals:
+		won = true
+		add_child(winscreen.instantiate())
