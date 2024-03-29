0eb2915e55a4a0e2a2846f1201fbc9370f26072a
Simon Rhodes <93493041+DripBoolean@users.noreply.github.com>
2024-03-29 14:25:56 -0400

    made projectiles somewhat transparent

--- scenes/player_and_parts/projectile.gd
+++ scenes/player_and_parts/projectile.gd
@@ -2,8 +2,11 @@ extends RigidBody3D
 
 var bounces = 0
 
+var time = 0.0
 
-func _process(_delta):
+func _process(delta):
+	time += delta
+	set_transparency((sin(time * 4) + 1.0) * 0.15)
 	if self.get_contact_count() > 0:
 		bounces += 1
 	if bounces > 5:
@@ -24,3 +27,8 @@ func _on_area_3d_area_entered(area):
 	if area.is_in_group("enemy"):
 		area.take_damage()
 		self.destroy()
+
+func set_transparency(value):
+	$MeshInstance3D.transparency = value
+	$MeshInstance3D2.transparency = value
+	$MeshInstance3D3.transparency = value
